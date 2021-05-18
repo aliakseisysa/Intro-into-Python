41c2d689edae8cde8e2c1272f0a90ed556b886b9
aliakseisysa <aliaksei.sysa@gmail.com>
2021-05-19 00:32:33 +0300

    Add files via upload

---
+++ Task_3.py
@@ -0,0 +1,2 @@
+any_number = int(input("Enter any number between 0 and 9: "))
+print(f"Sum of {str(any_number)}, {str(any_number) + str(any_number)} and {str(any_number) + str(any_number) + str(any_number)} = {any_number + (10 * any_number + any_number) + (100 * any_number + 10 * any_number + any_number)}")
