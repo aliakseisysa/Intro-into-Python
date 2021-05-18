8ed7455f3948aaef27ac50e55920234d07c380de
aliakseisysa <aliaksei.sysa@gmail.com>
2021-05-19 00:44:11 +0300

    Delete Task_3.py

--- Task_3.py
+++
@@ -1,2 +0,0 @@
-any_number = int(input("Enter any number between 0 and 9: "))
-print(f"Sum of {str(any_number)}, {str(any_number) + str(any_number)} and {str(any_number) + str(any_number) + str(any_number)} = {any_number + (10 * any_number + any_number) + (100 * any_number + 10 * any_number + any_number)}")
