6e0e00e0e12a21e7e45f0ac87be5dcee6c1f3ca5
aliakseisysa <aliaksei.sysa@gmail.com>
2021-05-19 00:43:18 +0300

    Delete Task_1.py

--- Task_1.py
+++
@@ -1,9 +0,0 @@
-my_var_1 = 6
-my_var_2 = my_var_1 + 18
-print(my_var_2)
-
-age = int(input("Enter your age (in years): "))
-print("I`m", age, "years")
-
-name = input("Enter your name: ")
-print("My name is", name)
